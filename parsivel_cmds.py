3a7ba9890c81225cd3b14ffe0a3cded9e2c4e292
agoncalvesdeol <a.i.castro@tudelft.nl>
2022-11-10 12:53:37 +0100

    reset_parsivel.py sets up user defined telegram

--- parsivel_cmds.py
+++ parsivel_cmds.py
@@ -1,3 +1,13 @@
+from datetime import datetime
+from pathlib import Path
+from  util_functions import yaml2dict
+
+
+print(__file__)
+wd = Path(__file__).parent 
+config_dict = yaml2dict(path = wd / 'config.yml')
+
+
 parsivel_telegram_command = 'CS/RA\r'.encode('utf-8')  # Asks the Parsivel to read out all fields
 parsivel_request_field_61 = 'CS/R/61\r'.encode('utf-8')  # Asks the Parsivel to read out field 61
 parsivel_request_field_90 = 'CS/R/91\r'.encode('utf-8')  # Asks the Parsivel to read out field 61
@@ -8,11 +18,12 @@ parsivel_user_telegram = 'CS/M/M/1\r'.encode('utf-8')  # The Parsivel broadcasts
 parsivel_set_telegram_list = 'CS/M/S/%01;%02;%03;%04;%05;%06;%07;%08;%09;%10;%11;%12;%13;%14;%15;%16;%17;%18;%19;%20;%21;%22;%23;%24;%25;%26;%27;%28;%30;%31;%32;%33;%34;%35;%60;%90;%91;%93\r'.encode('utf-8')  # Defines which fields are in the telegram # DONE = MIGRATED TO SCRIPTS
 parsivel_current_configuration = 'CS/L\r'.encode('utf-8')  # Outputs current configuration  # DONE = MIGRATED TO SCRIPTS
 parsivel_impulse_mode = 'CS/I/60\r'.encode('utf-8')  # Turns poll mode off
-#parsivel_set_time = 'CS/T/' + time.strftime("%H =%M =%S") + '\r'  # Sets the time on the Parsivel to the time on the Pi
-#parsivel_set_date = 'CS/D/' + time.strftime("%d.%m.%Y") + '\r'  # Sets the date on the Parsivel to the date on the Pi
-# parsivel_set_name = 'CS/K/' + Parsivel_name + '\r'  # Sets the name of the Parsivel, maximum 10 characters
-# parsivel_set_ID = 'CS/J/' + Parsivel_ID + '\r'  # Sets the ID of the Parsivel, maximum 4 numerical characters
+parsivel_set_time = ('CS/T/' + (datetime.utcnow().strftime("%H =%M =%S")) + '\r').encode('utf-8')  # Sets the time on the Parsivel to the time on the Pi
+parsivel_set_date = ('CS/D/' + (datetime.utcnow().strftime("%d.%m.%Y")) + '\r').encode('utf-8')  # Sets the date on the Parsivel to the date on the Pi
+parsivel_set_name = ('CS/K/' + config_dict['Parsivel_name'] + '\r').encode('utf-8')  # Sets the name of the Parsivel, maximum 10 characters
+parsivel_set_ID = ('CS/J/' + config_dict['Parsivel_ID'] + '\r').encode('utf-8')  # Sets the ID of the Parsivel, maximum 4 numerical characters
 parsivel_reset_factory_settings = 'CS/F/1\r'.encode('utf-8')  # Resets the Parsivel to factory settings.
 parsivel_real_time = 'CS/U\r'.encode('utf-8')
-#parsivel_set_real_time = 'CS/U/' + time.strftime("%d.%m.%Y ") + time.strftime("%H =%M =%S") + '\r'
+parsivel_set_real_time = ('CS/U/' + datetime.utcnow().strftime("%d.%m.%Y ") + datetime.utcnow().strftime("%H =%M =%S") + '\r').encode('utf-8')
 parsivel_restart = 'CS/Z/1\r'.encode('utf-8')
+
--- reset_parsivel.py
+++ reset_parsivel.py
@@ -1,5 +1,3 @@
-
-
 import serial
 from time import sleep
 from pathlib import Path
@@ -10,7 +8,6 @@ from parsivel_cmds import *
 print(__file__)
 wd = Path(__file__).parent 
 config_dict = yaml2dict(path = wd / 'config.yml')
-
 
 def init_serial(port: str, baud: int):
     try:
@@ -24,18 +21,24 @@ def init_serial(port: str, baud: int):
 parsivel = init_serial(port=config_dict['port'], baud=config_dict['baud'])
 parsivel.reset_input_buffer()  # Flushes input buffer
 
-parsivel.write(parsivel_ott_telegram) # Writes the Parsivel OTT telegram command to the Parsivel
+# parsivel.write(parsivel_ott_telegram) # Writes the Parsivel OTT telegram command to the Parsivel
+parsivel.write(parsivel_user_telegram)
 parsivel.write(parsivel_set_telegram_list) # Writes the parsivel user telegram string to the Parsivel
 parsivel.write(parsivel_telegram_command)
 parsivel.write(parsivel_command_list)
-parsivel.write(parsivel_user_telegram)
 parsivel.write(parsivel_telegram_start)
 parsivel.write(parsivel_current_configuration)
 parsivel.write(parsivel_impulse_mode)
-# parsivel.write(parsivel_set_name)
-# parsivel.write(parsivel_set_ID)
+parsivel.write(parsivel_set_name)
+parsivel.write(parsivel_set_ID)
 parsivel.write(parsivel_real_time)
+parsivel.write(parsivel_set_time)
 parsivel.write(parsivel_request_field_90)
-# parsivel.write(parsivel_set_real_time)
+parsivel.write(parsivel_set_real_time)
 parsivel.write(parsivel_restart)
-parsivel.write('CS/R/19\r'.encode('utf-8'))
+parsivel.write('CS/R/19\r'.encode('utf-8')) # date and time start
+
+sleep(2)
+parsivel.write(parsivel_current_configuration) # ask parsivel for config
+for config_line in parsivel.readlines(): # print config
+    print(config_line)
