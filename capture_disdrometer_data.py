648b51cda01f1c18eb1b6df12a63163765bd3869
Andre Castro <a.i.castro@tudelft.nl>
2022-11-14 15:13:49 +0100

    Resolve "implement daily sub-directories for data"

--- capture_disdrometer_data.py
+++ capture_disdrometer_data.py
@@ -1,63 +1,28 @@
 import time
 import csv
-import os
-import sys
 from datetime import datetime
 from pathlib import Path
-from typing import Dict
-import serial
-from  util_functions import yaml2dict
+from  util_functions import yaml2dict, create_dir, create_new_csv, binary2list, init_serial
 from parsivel_cmds import *
 from log import log 
-
-def init_serial(port: str, baud: int):
-    try:
-        parsivel = serial.Serial(port, baud, timeout=1)  # Defines the serial port
-        logger.info(msg=f'Connected to parsivel, via: {parsivel}')
-    except Exception as e:
-        logger.error(msg=e)
-        print(e)
-        sys.exit()
-    parsivel.reset_input_buffer()              
-    return parsivel
-
-def binary2list(binarystr, spliter):
-    binarystr = binarystr.decode('utf-8') 
-    binarystr = binarystr.replace('\n','').replace('\r','') # strip non-printing chars
-    binarystr_list = binarystr.split(spliter) 
-    return binarystr_list  
 
 print('starting script')
 
 wd = Path(__file__).parent 
-print(wd)
 config_dict = yaml2dict(path = wd / 'config.yml')
 
 # set up log
 log_dir = wd / config_dict['log_dir']
-if not os.path.exists(log_dir):
-    os.mkdir(log_dir)
+created_log_dir = create_dir(log_dir)
 log_file = log_dir / 'log.json'
 logger = log(log_path=log_file, 
             log_name=f"{config_dict['script_name']}: {config_dict['Parsivel_name']}")  
-logger.info(msg=f"Starting {config_dict['script_name']} for {config_dict['Parsivel_name']}")
+logger.info(msg=f"Starting {__file__} for {config_dict['Parsivel_name']}")
+print(f'{__file__} running\nLogs written to {log_dir}')
 
-# set up data dir
-data_dir = wd / config_dict['data_dir']
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
-print(f'{__file__} running\nLogs written to {log_dir}\nData written to {data_dir}')
 
-def create_new_csv(csv_path, headers):
-    if not os.path.exists(csv_path):
-        logger.info(msg=f"Creating {csv_path}")
-        with open(csv_path, "w") as f:
-            writer = csv.writer(f, delimiter=";")
-            writer.writerow(headers)
-            
-# intiated serial connection
-parsivel = init_serial(port=config_dict['port'], baud=config_dict['baud'])
-
+# # intiated serial connection
+parsivel = init_serial(port=config_dict['port'], baud=config_dict['baud'], logger=logger)
 # setup parsivel config commands 
 parsivel.write(parsivel_user_telegram) # set up parsivel: to send user defined telegram 
 time.sleep(2) 
@@ -70,18 +35,29 @@ for config_line in parsivel.readlines(): # print config
 
 while True:
     try:
-        parsivel_lines = parsivel.readlines()  # Reads the output the serial communication
+        # time
         now_utc = datetime.utcnow()
         now_utc_iso = now_utc.isoformat()
         now_utc_ymd = now_utc.strftime("%Y%m%d")
+        # daily data dirs
+        data_dir = wd / config_dict['data_dir'] / now_utc_ymd # set up data dir
+        created_data_dir = create_dir(data_dir)
+        if created_data_dir:
+            logger.info(msg=f'Created data directory: {data_dir}')
         parsivel_set_telegram_list_str = parsivel_set_telegram_list.decode('utf-8')
         parsivel_set_telegram_list_str = parsivel_set_telegram_list_str.replace('CS/M/S/', '').replace('\r','').replace('%', 'Field_')
         headers = ['Timestamp (UTC)']+ parsivel_set_telegram_list_str.split(';')
         filename = f"{now_utc_ymd}_{config_dict['Parsivel_name']}.csv"
-        create_new_csv(csv_path=data_dir / filename, headers=headers) # if does not exist
-        filename_field_d61 = f"{now_utc_ymd}_{config_dict['Parsivel_name']}_field61.csv"
-        create_new_csv(csv_path=data_dir / filename_field_d61, headers=['Timestamp (UTC)','Particle_size', 'Particle_speed']) # if does not exist
-
+        created_new_csv = create_new_csv(csv_path=data_dir / filename, headers=headers)
+        # daily CSVs
+        if created_new_csv:
+            logger.info(msg=f'Created CSV: {data_dir / filename}')
+        filename_field_61 = f"{now_utc_ymd}_{config_dict['Parsivel_name']}_field61.csv"
+        created_new_csv_f61 = create_new_csv(csv_path=data_dir / filename_field_61, headers=['Timestamp (UTC)','Particle_size', 'Particle_speed'])
+        if created_new_csv_f61:
+            logger.info(msg=f'Created CSV: {data_dir / filename_field_61}')
+        # Telegram processing
+        parsivel_lines = parsivel.readlines()  # Reads the output the serial communication
         if len(parsivel_lines) == 1 and len(parsivel_lines[0]) >= 20:
             # single message with all fields, except 61
             parsivel_str_list = binary2list(binarystr=parsivel_lines[0], spliter=';')
@@ -92,16 +68,17 @@ while True:
             parsivel.write(parsivel_request_field_61)  # request field 61
         elif len(parsivel_lines) > 1:
             # field 61 condition
-            with open(data_dir / filename_field_d61, "a") as g:  # 61
+            with open(data_dir / filename_field_61, "a") as g:  # 61
                 writer = csv.writer(g, delimiter=";")
                 for line in parsivel_lines:
                     if len(line) > 5 and len(line) < 20:
                         # TODO: process parsivel_lines to str and remove non-printing chars
                         parsivel_str_list = binary2list(binarystr=line, spliter=';')
                         writer.writerow([now_utc_iso] + parsivel_str_list)
-                        logger.info(msg=f'Written row to {filename_field_d61} {parsivel_str_list}')
+                        logger.info(msg=f'Written row to {filename_field_61} {parsivel_str_list}')
     except Exception as e:
         if hasattr(e, 'message'):
             print(e.message)
+            logger.error(msg=e.message)
         else:
             print(e)
