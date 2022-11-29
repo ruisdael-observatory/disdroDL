912b4d73c2312bed09f26e097a7e7fa821b2aa6c
Andre Castro <a.i.castro@tudelft.nl>
2022-11-29 15:52:57 +0100

    reset without sleep

--- reset_parsivel.py
+++ reset_parsivel.py
@@ -1,4 +1,5 @@
 import serial
+import sys
 from time import sleep
 from pathlib import Path
 from  util_functions import yaml2dict
@@ -21,32 +22,19 @@ parsivel = init_serial(port=config_dict['port'], baud=config_dict['baud'])
 parsivel.reset_input_buffer()  # Flushes input buffer
 
 # parsivel.write(parsivel_ott_telegram) # Writes the Parsivel OTT telegram command to the Parsivel
-sleep(1)
 parsivel.write(parsivel_user_telegram)
-sleep(1)
 parsivel.write(parsivel_set_telegram_list) # Writes the parsivel user telegram string to the Parsivel
-sleep(1)
 parsivel.write(parsivel_telegram_command)
-sleep(1)
 parsivel.write(parsivel_command_list)
-sleep(1)
 parsivel.write(parsivel_telegram_start)
-sleep(1)
 parsivel.write(parsivel_impulse_mode)
-sleep(1)
 parsivel.write(parsivel_set_station_name)
-sleep(1)
 parsivel.write(parsivel_set_ID)
-sleep(1)
 parsivel.write(parsivel_request_field_90)
-sleep(1)
 parsivel.write(parsivel_set_real_time)
-sleep(1)
 parsivel.write(parsivel_restart)
-sleep(1)
 parsivel.write('CS/R/19\r'.encode('utf-8')) # date and time start
 
-sleep(2)
 parsivel.write(parsivel_current_configuration) # ask parsivel for config
 for config_line in parsivel.readlines(): # print config
     print(config_line)
