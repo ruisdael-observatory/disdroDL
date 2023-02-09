a5eb6adaae55b6b456815bd83030f3acec5340b7
Andre Castro <a.i.castro@tudelft.nl>
2023-02-09 16:52:55 +0100

    typo

--- poll.py
+++ poll.py
@@ -41,7 +41,7 @@ parsivel.write('CS/M/M/1\r\n'.encode('utf-8')) # User telegram:
 while true:
     parsivel.write('CS/M/S/%01,%02,%03,%04,%05,%06,%07,%08,%09,%10,%11,%12,%13,%14,%15,%16,%17,%18,%20,%21,%22,%23,%24,%25,%26,%27,%28,%30,%31,%32,%33,%34,%35,%60,\r\n'.encode('utf-8'))
     sleep(float(0.5))
-    telegram_single values=parsivel.readline()
-    print(telegram_single)
+    telegram_single_values=parsivel.readline()
+    print(telegram_single_values)
     sleep(60)
 
