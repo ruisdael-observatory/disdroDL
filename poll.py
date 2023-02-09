7859266e096d4b985138a27e1b9b6d16e8b72302
Andre Castro <a.i.castro@tudelft.nl>
2023-02-09 17:05:04 +0100

    wait between format and poll

--- poll.py
+++ poll.py
@@ -40,11 +40,10 @@ parsivel.write('CS/M/M/1\r\n'.encode('utf-8')) # User defined telegram
 
 while True:
     parsivel.write('CS/M/S/%01,%02,%03,%04,%05,%06,%07,%08,%09,%10,%11,%12,%13,%14,%15,%16,%17,%18,%20,%21,%22,%23,%24,%25,%26,%27,%28,%30,%31,%32,%33,%34,%35,%60,\r\n'.encode('utf-8'))
-    sleep(float(0.5))
+    sleep(1)
     parsivel.write('CS/P\r\n'.encode('utf-8'))
-    sleep(float(0.5))
+    sleep(1)
     telegram_single_values=parsivel.readline()
-    print(telegram_single_values)
-    print('ending iteration')
+    print('telegram:', telegram_single_values)
     sleep(60)
 
