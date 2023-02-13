39a9564c24121ad6f213821cc234c11a791aaea2
Andre Castro <a.i.castro@tudelft.nl>
2023-02-13 14:22:32 +0100

    svfs_prefix

--- capture_disdrometer_data.py
+++ capture_disdrometer_data.py
@@ -52,7 +52,7 @@ while True:
             # request telegram
             svfs = '%01,%02,%03,%04,%05,%06,%07,%08,%09,%10,%11,%12,%13,%14,%15,%16,%17,%18,%20,%21,%22,%23,%24,%25,%26,%27,%28,%30,%31,%32,%33,%34,%35,%60,'
             svfs_prefix = 'SVFs:'  # Single Value Fields; for identification 
-            svfs_cmd = 'CS/M/S/' + prefix
+            svfs_cmd = 'CS/M/S/' + svfs_prefix
             parsivel.write(svfs_cmd + svfs + '\nF90:%90,\nF91:%91,\nF93:%93,\nF61:%61;\r\n'.encode('utf-8'))
             sleep(1)
             parsivel.write('CS/P\r\n'.encode('utf-8'))
