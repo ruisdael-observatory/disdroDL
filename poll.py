683018851a7fd0dd7f72a9cda7226843cca3123d
agoncalvesdeol <a.i.castro@tudelft.nl>
2023-02-13 12:21:03 +0000

    sleep 1sec before polling

--- poll.py
+++ poll.py
@@ -40,21 +40,22 @@ parsivel.write('CS/M/M/1\r\n'.encode('utf-8')) # User defined telegram
 
 flag_zero_seconds = False
 while True:
-    now_min_secs = datetime.utcnow().strftime("%M:%S")
-    now_min_secs = now_min_secs.split(":")
-    if int(now_min_secs[1]) == 0 and flag_zero_seconds == False:
-        print(now_min_secs, datetime.utcnow().strftime("%M:%S"))
+    now_hour_min_secs = datetime.utcnow().strftime("%H:%M:%S")
+    now_hour_min_secs = now_hour_min_secs.split(":")
+    if int(now_hour_min_secs[2]) == 0 and flag_zero_seconds == False:
+        print(now_hour_min_secs, datetime.utcnow().strftime("%H:%M:%S"))
         flag_zero_seconds = True
 
         parsivel.write('CS/M/S/SFs:%01,%02,%03,%04,%05,%06,%07,%08,%09,%10,%11,%12,%13,%14,%15,%16,%17,%18,%20,%21,%22,%23,%24,%25,%26,%27,%28,%30,%31,%32,%33,%34,%35,%60,\nF90:%90,\nF91:%91,\nF93:%93,\nF61:%61;\r\n'.encode('utf-8'))
-        sleep(1) # can i remove this ?
+        sleep(1)
         parsivel.write('CS/P\r\n'.encode('utf-8'))
         telegram_single_values=parsivel.readlines()
         for index, item in enumerate(telegram_single_values):
             print(index, item)
         print('\n')
 
-    elif int(now_min_secs[1]) != 0 and flag_zero_seconds == True:
+    elif int(now_hour_min_secs[2]) != 0 and flag_zero_seconds == True:
+
         # once we passed 00secs 
         # reset flag_zero_seconds
         flag_zero_seconds = False
