6b9d0667849e50ce286f85f9f88c588c6825bc25
Andre Castro <a.i.castro@tudelft.nl>
2023-02-13 13:41:38 +0100

    wip:write to CSV

--- capture_disdrometer_data.py
+++ capture_disdrometer_data.py
@@ -35,40 +35,54 @@ while True:
         now_utc = datetime.utcnow()
         now_hour_min_secs = now_utc.strftime("%H:%M:%S")
         now_hour_min_secs = now_hour_min_secs.split(":")
+
         if int(now_hour_min_secs[2]) == 0 and flag_zero_seconds == False:
             print(now_hour_min_secs, datetime.utcnow().strftime("%H:%M:%S"))
             flag_zero_seconds = True
-
-            # create dir
+            # dates
+            now_utc_iso = now_utc.isoformat()
             now_utc_ym = now_utc.strftime("%Y%m")
+            now_utc_ymd = now_utc.strftime("%Y%m%d")
+            # create dir
             data_dir = Path(config_dict['data_dir']) / now_utc_ym # create monthly data dir
             created_data_dir = create_dir(data_dir)
             if created_data_dir:
                 logger.info(msg=f'Created data directory: {data_dir}')
+
             # request telegram
             parsivel.write('CS/M/S/SFs:%01,%02,%03,%04,%05,%06,%07,%08,%09,%10,%11,%12,%13,%14,%15,%16,%17,%18,%20,%21,%22,%23,%24,%25,%26,%27,%28,%30,%31,%32,%33,%34,%35,%60,\nF90:%90,\nF91:%91,\nF93:%93,\nF61:%61;\r\n'.encode('utf-8'))
             sleep(1)
             parsivel.write('CS/P\r\n'.encode('utf-8'))
             telegram_single_values=parsivel.readlines()
+
+            # create CSV
+            # headers = ['Timestamp (UTC)']+ parsivel_set_telegram_list_str.split(';')
+            filename = f"{now_utc_ymd}_{config_dict['station_site']}-{config_dict['station_name']}_{config_dict['Parsivel_name']}.csv"
+            created_new_csv = create_new_csv(csv_path=data_dir / filename) # headers=headers)
+            # daily CSVs
+            if created_new_csv:
+                logger.info(msg=f'Created CSV: {data_dir / filename}')
+
             for index, item in enumerate(telegram_single_values):
                 print(index, item)
             print('\n')
 
-        elif int(now_hour_min_secs[2]) != 0 and flag_zero_seconds == True:
 
-            # once we passed 00secs 
-            # reset flag_zero_seconds
+        elif int(now_hour_min_secs[2]) != 0 and flag_zero_seconds == True:
+            # once we passed 00secs: reset flag_zero_seconds
             flag_zero_seconds = False
-        sleep(1)
     except Exception as e:
         if hasattr(e, 'message'):
             print(e.message)
             logger.error(msg=e.message)
         else:
             print(e)
+    sleep(1)
 
 
 # TODO:
 # * check how field 61 is written 
 # * monthly dir creation
 # * write to CSV
+# * CSV headers
+# * write to several CSVs
