7bd03402c05b1eb6e64a36e664663bde01b441e5
Andre Castro <a.i.castro@tudelft.nl>
2023-02-21 12:24:57 +0100

    F61 headers

--- capture_disdrometer_data.py
+++ capture_disdrometer_data.py
@@ -71,7 +71,9 @@ try:
                     headers = csv_headers(sfvs_telegram_resquest=svfs, config_dict=config_dict)
                     print('headers:', headers)
                 elif suffix == 'F61':
-                    headers = ['timestamp', f"{config_dict['telegram_fields']['61size']['name']}", f"({config_dict['telegram_fields']['61speed']['unit']})"]                    
+                    headers = ['timestamp', 
+                               f"{config_dict['telegram_fields']['61size']['name']} ({config_dict['telegram_fields']['61size']['unit']})", 
+                               f"{config_dict['telegram_fields']['61speed']['name']} ({config_dict['telegram_fields']['61speed']['unit']})"]                    
                 else:
                     headers = []
                 created_new_csv = create_new_csv(csv_path=data_dir / csvs_suffixes[suffix], headers=headers)
