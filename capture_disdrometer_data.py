3a0e041b60fdf2788f1011978e5421762dddc877
Andre Castro <a.i.castro@tudelft.nl>
2023-02-23 17:29:28 +0100

    refactoring complete

--- capture_disdrometer_data.py
+++ capture_disdrometer_data.py
@@ -53,9 +53,9 @@ try:
                                 data_dir=data_dir,
                                 data_fn_start=fn_start)    
             telegram.create_csv_headers(sfvs_telegram_resquest=svfs, config_dict=config_dict)
-            telegram.capture_prefixes_and_data()               
-            telegram.append_data_to_csv()
-
+            telegram.capture_prefixes_and_data()
+            for prefix in prefixes_list:
+                telegram.append_data_to_csv(prefix=prefix)
 
         elif int(now_utc.time_list[2]) != 0 and flag_zero_seconds == True:
             # once we passed 00secs: reset flag_zero_seconds
