e9559a80ee760eb5c7d0d8c5d8e34d19213f9433
Andre Castro <a.i.castro@tudelft.nl>
2023-05-24 15:04:47 +0200

    telegram_line_decoded

--- modules/util_functions.py
+++ modules/util_functions.py
@@ -31,7 +31,8 @@ def capture_telegram_prfx_vars(telegram_line):
         Through regex the prefix and values are captures from telegram str
         Note:  F61 will be an exception since its values are multiline
     '''
-    prefix_match = re.match(r'(^.{3,4}):(.*?)$', telegram_line.decode('utf-8')) 
+    telegram_line_decoded = telegram_line.decode('utf-8')
+    prefix_match = re.match(r'(^.{3,4}):(.*?)$', telegram_line_decoded) 
     if prefix_match:
         prefix = prefix_match.group(1)
         values = prefix_match.group(2)
