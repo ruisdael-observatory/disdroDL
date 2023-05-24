1ffa82d51b3b810aa20191cb9ac8be76e7e82494
Andre Castro <a.i.castro@tudelft.nl>
2023-05-24 12:54:27 +0200

    logger included

--- debug.py
+++ debug.py
@@ -1,9 +1,15 @@
 from pathlib import Path
 from time import sleep
-from modules.util_functions import yaml2dict, create_dir, init_serial, parsivel_start_sequence
+from modules.util_functions import yaml2dict, create_dir, init_serial, parsivel_start_sequence, create_logger
 from modules.classes import NowTime
 wd = Path(__file__).parent 
 config_dict = yaml2dict(path = wd / 'config.yml')
+
+
+logger = create_logger(log_dir=Path(config_dict['log_dir']), 
+                       script_name=config_dict['script_name'], 
+                       parsivel_name=config_dict['Parsivel_name'])
+logger.info(msg=f"Starting {__file__} for {config_dict['Parsivel_name']}")
 
 svfs = '%01;%02;%03;%04;%05;%06;%07;%08;%09;%10;%11;%12;%13;%14;%15;%16;%17;%18;%20;%21;%22;%23;%24;%25;%26;%27;%28;%30;%31;%32;%33;%34;%35;%60;'
 user_telegram_str = f'CS/M/S/SVFS:' 
