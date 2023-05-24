f1678bb036dd062ff6ddadbb670bb113be141a69
Andre Castro <a.i.castro@tudelft.nl>
2023-05-24 13:33:09 +0200

    requesting F24 and F26; only writing Pool mode once

--- debug.py
+++ debug.py
@@ -11,13 +11,19 @@ logger = create_logger(log_dir=Path(config_dict['log_dir']),
                        parsivel_name=config_dict['Parsivel_name'])
 logger.info(msg=f"Starting {__file__} for {config_dict['Parsivel_name']}")
 
-svfs = '%01;%02;%03;%04;%05;%06;%07;%08;%09;%10;%11;%12;%13;%14;%15;%16;%17;%18;%20;%21;%22;%23;%24;%25;%26;%27;%28;%30;%31;%32;%33;%34;%35;%60;'
+svfs = '%24;%26' # 24 - Absolute Rain amount; 26 - PCB temp
+# '%01;%02;%03;%04;%05;%06;%07;%08;%09;%10;%11;%12;%13;%14;%15;%16;%17;%18;%20;%21;%22;%23;%24;%25;%26;%27;%28;%30;%31;%32;%33;%34;%35;%60;'
 user_telegram_str = f'CS/M/S/SVFS:' 
 user_telegram_str = (user_telegram_str + svfs + '\r').encode('utf-8')
 
 # Serial connection
 parsivel = init_serial(port=config_dict['port'], baud=config_dict['baud'], logger=logger)  # initiate serial connection
 parsivel_start_sequence(serialconnection=parsivel, config_dict=config_dict, logger=logger)
+
+sleep(1)
+parsivel.write('CS/P\r\n'.encode('utf-8')) # poll mode
+sleep(2)
+
 
 flag_zero_seconds = False
 while True:
@@ -30,7 +36,7 @@ while True:
         # Request telegram:
         parsivel.write(user_telegram_str)  # string format
         sleep(1)
-        parsivel.write('CS/P\r\n'.encode('utf-8')) # poll
+        # parsivel.write('CS/P\r\n'.encode('utf-8')) # poll
         lines = parsivel.readlines()
         print(f'Lines: {lines}')
     elif int(now_utc.time_list[2]) != 0 and flag_zero_seconds == True:
