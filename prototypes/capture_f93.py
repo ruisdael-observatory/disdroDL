c6ab525958ca5b737cbc2e51b42be8367559817a
Andre Castro <a.i.castro@tudelft.nl>
2023-05-25 11:36:00 +0200

    capture f93 w Windows-1252 encoding

--- prototypes/capture_f93.py
+++ prototypes/capture_f93.py
@@ -1,15 +1,16 @@
 import serial
 from time import sleep
 
+encoding = 'Windows-1252'  # 'utf-8'
 
 parsivel = serial.Serial( '/dev/ttyUSB0', 19200, timeout=1)  # Defines the serial port
 parsivel.reset_input_buffer()  # Flushes input buffer
 
 sleep(2)
-parsivel.write('CS/P\r\n'.encode('utf-8'))  # needed:yes
+parsivel.write('CS/P\r\n'.encode(encoding))  # needed:yes
 sleep(2) # needed?
 
-user_telegram_str = (f'CS/M/S/%93;\r').encode('utf-8')
+user_telegram_str = (f'CS/M/S/%93;\r').encode(encoding)
 parsivel.write(user_telegram_str)  # string format
 parsivel_lines = parsivel.readlines()
 print(parsivel_lines)
