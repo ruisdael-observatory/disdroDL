4bb7c730b3292ab99526dc1b461e2af22b7a91e7
Andre Castro <a.i.castro@tudelft.nl>
2023-05-25 11:21:56 +0200

    prototypes/capture_f93.py

--- prototypes/capture_f93.py
+++ prototypes/capture_f93.py
@@ -5,8 +5,9 @@ from time import sleep
 parsivel = serial.Serial( '/dev/ttyUSB0', 19200, timeout=1)  # Defines the serial port
 parsivel.reset_input_buffer()  # Flushes input buffer
 
-# parsivel.write('CS/P\r\n'.encode('utf-8'))  # needed:yes
-sleep(1) # needed?
+sleep(2)
+parsivel.write('CS/P\r\n'.encode('utf-8'))  # needed:yes
+sleep(2) # needed?
 
 user_telegram_str = (f'CS/M/S/%93;\r').encode('utf-8')
 parsivel.write(user_telegram_str)  # string format
