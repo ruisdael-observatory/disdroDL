34a9fffcbc62ad459217d6104a434ab0089c37d4
Andre Castro <a.i.castro@tudelft.nl>
2023-05-25 11:37:43 +0200

    capture f93 w Windows-1252 encoding

--- prototypes/capture_f93.py
+++ prototypes/capture_f93.py
@@ -14,3 +14,8 @@ user_telegram_str = (f'CS/M/S/%93;\r').encode(encoding)
 parsivel.write(user_telegram_str)  # string format
 parsivel_lines = parsivel.readlines()
 print(parsivel_lines)
+
+f_93_str = parsivel_lines[0].decode(encoding='Windows-1252', errors='strict')
+print(f_93_str)
+f_93_list = f_93_str.split(';')
+print('len F93:', len(f_93_list))
