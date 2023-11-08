c0cb876de9eafec3d2ff62dcabcff52eca43979f
Andre Castro <a.i.castro@tudelft.nl>
2023-11-08 15:17:54 +0100

    rm prototypes

--- prototypes/prototype_parse_yml.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-from pprint import pprint
-from  util_functions import yaml2dict
-
-wd = Path(__file__).parent 
-config_dict = yaml2dict(path = wd / 'config.yml')
-pprint(config_dict)
-
-pprint(config_dict['telegram_fields']['01'])
