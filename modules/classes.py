b65c47292f2cce35de35f557a8a56eb431cefd9d
Andre Castro <a.i.castro@tudelft.nl>
2023-12-07 09:43:04 +0100

    compress vars; not fletcher32 arg

--- modules/classes.py
+++ modules/classes.py
@@ -193,20 +193,21 @@ def set_netcdf_variable(key, one_var_dict, nc_group, timestamp, logger):
                 fill_value=-1,
                 compression=compression_method,
                 complevel=9,
-                shuffle=True,
-                fletcher32=True)
+                shuffle=True)
 
         elif len(one_var_dict['dimensions']) >= 1:
             if 'fill_value' in one_var_dict.keys():
                 fill_val = one_var_dict['fill_value']
             else:
-                fill_val = -1 
-            variable = nc_group.createVariable(one_var_dict['var_attrs']['standard_name'], 
-                                            one_var_dict['dtype'],
-                                            tuple([dim for dim in one_var_dict['dimensions']]),
-                                            compression=compression_method,
-                                            fill_value=fill_val,
-                                            )
+                fill_val = -1
+            variable = nc_group.createVariable(
+                one_var_dict['var_attrs']['standard_name'],
+                one_var_dict['dtype'],
+                tuple([dim for dim in one_var_dict['dimensions']]),
+                fill_value=fill_val,
+                compression=compression_method,
+                complevel=9,
+                shuffle=True)
         # fill predefine values
         if 'value' in one_var_dict.keys() and len(one_var_dict['value']) == 1:
             # use .assignValue() method for scalar values
