71ecf766137d5c499cabd92d94fbb5387352dc68
Andre Castro <a.i.castro@tudelft.nl>
2023-12-13 11:54:09 +0100

    sqldb: create + tests - using python sqlite3 instead of sqlalchemy

--- modules/sqldb.py
+++ modules/sqldb.py
@@ -1,30 +1,53 @@
-
-from sqlalchemy import FLOAT, Table, Column, Integer, String
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine
+import sqlite3
+# from sqlalchemy import FLOAT, Table, Column, Integer, String
+# from sqlalchemy.orm import declarative_base
+# from sqlalchemy import create_engine
 
 # telegram_fields = config_dict['telegram_fields'].keys()
-def create_db(dbpath, telegram_fields):
+
+
+def create_db(dbpath):
     '''
     create disdrodl.db
     with Table: disdrodl 
-    where telegram fields become columns 
-    extra columns: id, timestamp, parsivel_id are included 
-    '''
+    with columns id, timestamp, parsivel_id, telegram 
+    '''    
+    con = sqlite3.connect(str(dbpath))
+    cur = con.cursor()
+    cur.execute("""
+                CREATE TABLE IF NOT EXISTS disdrodl
+                (
+                    id INTEGER,
+                    timestamp REAL,
+                    datetime TEXT,
+                    parsivel_id TEXT,
+                    telegram TEXT
+                )
+                """)
+    con.commit()
 
-    Base = declarative_base()
-    disdrodl_table = Table(
-        "disdrodl",
-        Base.metadata,
-        *(Column(f'f_{column_name}', String, primary_key=False)
-        for column_name in telegram_fields)
-    )
-    disdrodl_table.append_column(Column('id', Integer,primary_key=True, autoincrement=True) )
-    disdrodl_table.append_column(Column('timestamp', FLOAT ) )
-    disdrodl_table.append_column(Column('datetime', String ) )
-    disdrodl_table.append_column(Column('parsivel_id', String))
-    print(disdrodl_table._columns)
 
-    engine = create_engine(f'sqlite:///{dbpath}', echo=False)
-    Base.metadata.create_all(engine)
+# def create_db(dbpath, telegram_fields):
+#     '''
+#     create disdrodl.db
+#     with Table: disdrodl 
+#     where telegram fields become columns 
+#     extra columns: id, timestamp, parsivel_id are included 
+#     '''
+
+#     Base = declarative_base()
+#     disdrodl_table = Table(
+#         "disdrodl",
+#         Base.metadata,
+#         *(Column(f'f_{column_name}', String, primary_key=False)
+#         for column_name in telegram_fields)
+#     )
+#     disdrodl_table.append_column(Column('id', Integer,primary_key=True, autoincrement=True) )
+#     disdrodl_table.append_column(Column('timestamp', FLOAT ) )
+#     disdrodl_table.append_column(Column('datetime', String ) )
+#     disdrodl_table.append_column(Column('parsivel_id', String))
+#     print(disdrodl_table._columns)
+
+#     engine = create_engine(f'sqlite:///{dbpath}', echo=False)
+#     Base.metadata.create_all(engine)
 
--- test_db.py
+++ test_db.py
@@ -1,9 +1,7 @@
-import pytest
 import os
+import sqlite3
+import pytest
 from pathlib import Path
-from random import randint
-from sqlalchemy import create_engine, inspect
-from sqlalchemy import text
 from modules.sqldb import create_db
 
 wd = Path().resolve()
@@ -12,41 +10,37 @@ db_path = wd / db_file
 if os.path.isfile(db_path):
     os.remove(db_path)
 
-random_telegram_fields = set([str(randint(1, 99)).zfill(2) for i in range(20)])
-print(random_telegram_fields)
+# # random_telegram_fields = set([str(randint(1, 99)).zfill(2) for i in range(20)])
+# print(random_telegram_fields)
 
 
 @pytest.fixture()
 def create_db_():
-    create_db(dbpath=db_file, telegram_fields=random_telegram_fields)
+    create_db(dbpath=db_file)
 
 
 def test_db_schema(create_db_):
-    engine = create_engine(f'sqlite:///{db_file}', echo=False)
-    with engine.connect() as conn:
-        inspector = inspect(engine)
-        for i, col in enumerate(inspector.get_columns('disdrodl')):
-            if i < len(random_telegram_fields):
-                assert col['name'] == f'f_{list(random_telegram_fields)[i]}'
-            elif i == len(random_telegram_fields):
-                assert col['name'] == 'id'
-            elif i == len(random_telegram_fields)+1:
-                assert col['name'] == 'timestamp'
-            elif i == len(random_telegram_fields)+2:
-                assert col['name'] == 'datetime'
-            elif i == len(random_telegram_fields)+3:
-                assert col['name'] == 'parsivel_id'
+    con = sqlite3.connect(str(db_file))
+    cur = con.cursor()
+    table_info = cur.execute("PRAGMA table_info('disdrodl');")
+    table_info_res = table_info.fetchall()
+    table_cols = ['id', 'timestamp', 'datetime', 'parsivel_id', 'telegram']
+    table_cols_dt = ['INTEGER', 'REAL', 'TEXT', 'TEXT', 'TEXT']
+    for i, col in enumerate(table_info_res):
+        print(col)
+        assert col[1] == table_cols[i]
+        assert col[2] == table_cols_dt[i]
 
 # TODO: insert data to  db
 
 
-def test_db_select(create_db_):
-    engine = create_engine(f'sqlite:///{db_file}', echo=False)
-    with engine.connect() as conn:
-        rs = conn.execute(text('SELECT * FROM disdrodl'))
-        # import pdb; pdb.set_trace()
-        rs.rowcount
-        rs.first()
+# def test_db_select(create_db_):
+#     engine = create_engine(f'sqlite:///{db_file}', echo=False)
+#     with engine.connect() as conn:
+#         rs = conn.execute(text('SELECT * FROM disdrodl'))
+#         # import pdb; pdb.set_trace()
+#         rs.rowcount
+#         rs.first()
 
         # for i, rs_item in enumerate(rs):
         #     print(i, rs_item)
