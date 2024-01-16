f47e8e4f6d48a2f100da15b41dc809987429ae26
Andre Castro <a.i.castro@tudelft.nl>
2024-01-16 10:31:43 +0100

    Resolve "rewrite: to write telegram to slqlite and export once to netcdf"

--- modules/util_functions.py
+++ modules/util_functions.py
@@ -4,21 +4,24 @@ import sys
 import serial
 from typing import Dict
 from time import sleep
-if __name__ == '__main__': 
-    from log import log 
-else:
-    from modules.log import log 
+from pathlib import Path
 
-def yaml2dict(path: str) -> Dict:
+if __name__ == '__main__':
+    from log import log
+else:
+    from modules.log import log
+
+
+def yaml2dict(path: Path) -> Dict:
     with open(path, 'r') as yaml_f:
         yaml_content = yaml_f.read()
         yaml_dict = yaml.safe_load(yaml_content)
     return yaml_dict
 
-def create_dir(path: str):
+
+def create_dir(path: Path):
     if not os.path.exists(path):
-        path.mkdir(parents=True)
-        # os.mkdir(path)
+        Path.mkdir(path, parents=True)
         created_dir = True
     else:
         created_dir = False
@@ -35,18 +38,21 @@ def init_serial(port: str, baud: int, logger):
         sys.exit()
     return parsivel
 
+
 def resetSerialBuffers(serial_connection):
     serial_connection.reset_input_buffer()
     sleep(1)
     serial_connection.reset_output_buffer()
+
 
 def interruptHandler(serial_connection, logger):
     msg = 'Interrupting execution'
     print(msg)
     logger.info(msg=msg)
     resetSerialBuffers(serial_connection=serial_connection)
-    serial_connection.close()    
-    
+    serial_connection.close()
+
+
 def create_logger(log_dir, script_name, parsivel_name):
     create_dir(log_dir)
     log_file = log_dir / f'log_{script_name}.json'
@@ -55,27 +61,32 @@ def create_logger(log_dir, script_name, parsivel_name):
     logger.info(msg=f"Starting {script_name} for {parsivel_name}")
     return logger
 
+
 def parsivel_start_sequence(serialconnection, config_dict, logger):
     logger.info(msg="Starting parsivel start sequence commands")
     serialconnection.reset_input_buffer()  # Flushes input buffer
-    parsivel_set_station_code = ('CS/K/' + config_dict['station_code'] + '\r').encode('utf-8')  # Sets the name of the Parsivel, maximum 10 characters
+    # Sets the name of the Parsivel, maximum 10 characters
+    parsivel_set_station_code = ('CS/K/' + config_dict['station_code'] + '\r').encode('utf-8')
     serialconnection.write(parsivel_set_station_code)
     sleep(1)
-    parsivel_set_ID = ('CS/J/' + config_dict['global_attrs']['sensor_name'] + '\r').encode('utf-8')  # Sets the ID of the Parsivel, maximum 4 numerical characters
+    # Sets the ID of the Parsivel, maximum 4 numerical characters
+    parsivel_set_ID = ('CS/J/' + config_dict['global_attrs']['sensor_name'] + '\r').encode('utf-8')
     serialconnection.write(parsivel_set_ID)
     sleep(2)
     parsivel_restart = 'CS/Z/1\r'.encode('utf-8')
     serialconnection.write(parsivel_restart)  # resets rain amount
     sleep(10)
-    parsivel_user_telegram = 'CS/M/M/1\r'.encode('utf-8')  # The Parsivel broadcasts the user defined telegram. # DONE = MIGRATED TO SCRIPTS
-    serialconnection.write(parsivel_user_telegram) 
+    # The Parsivel broadcasts the user defined telegram.
+    parsivel_user_telegram = 'CS/M/M/1\r'.encode('utf-8')
+    serialconnection.write(parsivel_user_telegram)
+
 
 def parsivel_reset(serialconnection, logger, factoryreset):
     logger.info(msg="Reseting Parsivel")
-    if factoryreset == True:
+    if factoryreset is True:
         parsivel_reset = 'CS/F/1\r'.encode('utf-8')
         serialconnection.write(parsivel_reset)
-    else: 
+    else:
         parsivel_restart = 'CS/Z/1\r'.encode('utf-8')  # restart
         serialconnection.write(parsivel_restart)
     sleep(5)
