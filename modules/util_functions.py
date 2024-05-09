54d284ff436d6a4fdf228514ba9c09a2c4851e5b
Noky Soekarman <N.P.Soekarman@student.tudelft.nl>
2024-05-09 11:04:26 +0200

    Refactored util_functions.py

--- modules/util_functions.py
+++ modules/util_functions.py
@@ -1,25 +1,38 @@
-import yaml
+"""
+Imports
+"""
 import os
 import sys
-import serial
 from time import sleep
 from pathlib import Path
 from typing import Dict, Union
+import serial
+import yaml
 
 if __name__ == '__main__':
-    from log import log
+    from log import log # pylint: disable=import-error
 else:
-    from modules.log import log
+    from modules.log import log # pylint: disable=import-error
 
 
 def yaml2dict(path: Path) -> Dict:
-    with open(path, 'r') as yaml_f:
+    """
+    This function reads a yaml file and returns a dictionary with all the field and values.
+    :param path: the path to the yaml file
+    :return: dictionary with all the field and values
+    """
+    with open(path, 'r') as yaml_f: # pylint: disable=unspecified-encoding
         yaml_content = yaml_f.read()
         yaml_dict = yaml.safe_load(yaml_content)
     return yaml_dict
 
 
 def create_dir(path: Path):
+    """
+    This function creates a directory if it does not exist.
+    :param path: the path to the directory
+    :return: True if the directory was created, False if it already existed
+    """
     if not os.path.exists(path):
         Path.mkdir(path, parents=True)
         created_dir = True
@@ -29,10 +42,17 @@ def create_dir(path: Path):
 
 
 def init_serial(port: str, baud: int, logger):
+    """
+    This function initiates a serial connection to the disdrometer.
+    :param port: the port to connect to
+    :param baud: the baud rate
+    :param logger: the logger object
+    :return: the serial connection object
+    """
     try:
         parsivel = serial.Serial(port, baud, timeout=1)  # Defines the serial port
         logger.info(msg=f'Connected to parsivel, via: {parsivel}')
-    except Exception as e:
+    except Exception as e: # pylint: disable=broad-exception-caught
         logger.error(msg=e)
         # print(e)
         sys.exit()
@@ -40,12 +60,21 @@ def init_serial(port: str, baud: int, logger):
 
 
 def resetSerialBuffers(serial_connection):
+    """
+    This function resets the input and output buffers of the serial connection.
+    :param serial_connection: the serial connection object that needs to be reset
+    """
     serial_connection.reset_input_buffer()
     sleep(1)
     serial_connection.reset_output_buffer()
 
 
 def interruptHandler(serial_connection, logger):
+    """
+    This function interrupts the execution of the serial connection.
+    :param serial_connection: the serial connection object to interrupt
+    :param logger: the logger object
+    """
     msg = 'Interrupting execution'
     print(msg)
     logger.info(msg=msg)
@@ -54,6 +83,13 @@ def interruptHandler(serial_connection, logger):
 
 
 def create_logger(log_dir, script_name, parsivel_name):
+    """
+    This function creates a logger object that logs to a file.
+    :param log_dir: directory of the log file
+    :param script_name: name of the script
+    :param parsivel_name: name of the disdrometer
+    :return: the logger object
+    """
     create_dir(log_dir)
     log_file = log_dir / f'log_{script_name}.json'
     logger = log(log_path=log_file,
@@ -63,6 +99,12 @@ def create_logger(log_dir, script_name, parsivel_name):
 
 
 def parsivel_start_sequence(serialconnection, config_dict, logger):
+    """
+    This function sends the start sequence commands to the parsivel disdrometer.
+    :param serialconnection: the serial connection object
+    :param config_dict: dict with the configuration settings
+    :param logger: the logger object
+    """
     logger.info(msg="Starting parsivel start sequence commands")
     serialconnection.reset_input_buffer()  # Flushes input buffer
     # Sets the name of the Parsivel, maximum 10 characters
@@ -82,10 +124,16 @@ def parsivel_start_sequence(serialconnection, config_dict, logger):
 
 
 def parsivel_reset(serialconnection, logger, factoryreset):
+    """
+    This function resets the parsivel disdrometer.
+    :param serialconnection: the serial connection object
+    :param logger: the logger object
+    :param factoryreset: if the factory reset should be performed
+    """
     logger.info(msg="Reseting Parsivel")
     if factoryreset is True:
-        parsivel_reset = 'CS/F/1\r'.encode('utf-8')
-        serialconnection.write(parsivel_reset)
+        parsivel_reset_code = 'CS/F/1\r'.encode('utf-8')
+        serialconnection.write(parsivel_reset_code)
     else:
         parsivel_restart = 'CS/Z/1\r'.encode('utf-8')  # restart
         serialconnection.write(parsivel_restart)
@@ -111,4 +159,3 @@ def unpack_telegram_from_db(telegram_str: str) -> Dict[str, Union[str, list]]:
             val = None
         telegram_dict[key] = val
     return telegram_dict
-
