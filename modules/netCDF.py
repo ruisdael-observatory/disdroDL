a9c7d8111975637ccdc8b19d77e95eff329c4ff1
Jesse Vleeschdraager <J.C.Vleeschdraager@student.tudelft.nl>
2024-05-16 14:47:37 +0200

    Changes to test the concept

--- modules/netCDF.py
+++ modules/netCDF.py
@@ -63,8 +63,8 @@ class NetCDF:
         # --- NetCDF variables in telegram_data ---
         for key in self.telegram_objs[0].telegram_data.keys(): # pylint: disable=too-many-nested-blocks
             if key in self.config_dict['telegram_fields'].keys() and \
-                    ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') != 'never') or
-                    (self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') == 'full')):
+                    ((self.full_version is True) or
+                    (self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') is True)):
                 field_dict = self.config_dict['telegram_fields'][key]
                 standard_name = field_dict['var_attrs']['standard_name']
                 netCDF_var = netCDF_rootgrp.variables[standard_name]
@@ -163,9 +163,8 @@ class NetCDF:
 
     def __set_netcdf_variable(self, key, one_var_dict, nc_group):
         self.logger.info(msg=f"creating netCDF variable {one_var_dict['var_attrs']['standard_name']}")
-        print(self.config_dict['telegram_fields'][key].get('include_in_nc'))
-        if ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') != 'never') or
-            (self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') == 'full')):
+        if ((self.full_version is True) or
+            (self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') is True)):
             if one_var_dict['dtype'] != 'S4':  # can't compress variable-length str variables
                 compression_method = 'zlib'
                 # compression_method = dict(zlib=True, shuffle=True, complevel=5)
