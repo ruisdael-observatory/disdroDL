421725e91c7cc242d30eb23c9a3b89bffd4db4aa
Vasil <v.chirov@student.tudelft.nl>
2024-05-16 19:22:15 +0200

    added documentation and fixed pylint

--- test_sensors_parsivel.py
+++ test_sensors_parsivel.py
@@ -1,17 +1,30 @@
-import sys
+"""
+Module for testing the parsivel class from sensors.py
+"""
 import unittest
 from unittest.mock import Mock, patch, call
 from modules.sensors import Parsivel
 
 
 class TestParsivel(unittest.TestCase):
+    """
+    Class for testing the Parsivel class
+    """
+
     def test___init__(self):
+        """
+        Constructor test
+        """
         parsivel = Parsivel()
         assert parsivel.serial_connection is None
         assert parsivel.get_type() == "parsivel"
 
     @patch('serial.Serial')
     def test_init_serial_connection_success(self, mock_serial):
+        """
+        Good weather test for the init_serial_connection_success function
+        :param mock_serial: Mock of the serial.Serial call
+        """
         mock_logger = Mock()
         parsivel_obj = Parsivel()
 
@@ -25,17 +38,30 @@ class TestParsivel(unittest.TestCase):
     @patch('serial.Serial', side_effect=Exception('Test'))
     @patch('sys.exit', side_effect=Exception('Exit'))
     def test_init_serial_connection_exception(self, mock_serial, mock_exit):
+        """
+        Bad weather test for the init_serial_connection_success function
+        :param mock_serial: Mock of the serial.Serial call
+                            with an Exception side effect
+        :param mock_exit: Mock of the sys.exit call with
+                            an Exception side effect, used
+                            to prevent the execution of the
+                            last line of the function under test
+        """
         mock_logger = Mock()
         parsivel_obj = Parsivel()
         try:
             parsivel_obj.init_serial_connection(port="test", baud=1, logger=mock_logger)
-        except Exception as e:  # pylint: disable=broad-exception-caught
+        except Exception:  # pylint: disable=broad-exception-caught
             mock_serial.assert_called_once()
             mock_logger.error.assert_called_once()
             mock_exit.assert_called_once()
 
     @patch('time.sleep', return_value=None)
     def test_sensor_start_sequence(self, mock_sleep):
+        """
+        Test for the sensor_start_sequence function
+        :param mock_sleep: Mock of the time.sleep call
+        """
         mock_logger = Mock()
         mock_serial_connection = Mock()
         parsivel_obj = Parsivel()
@@ -49,7 +75,7 @@ class TestParsivel(unittest.TestCase):
             }
         }
 
-        expected_calls = [
+        expected_calls_write = [
             call(b'CS/K/STATION1\r', mock_logger),
             call(b'CS/J/1234\r', mock_logger),
             call(b'CS/Z/1\r', mock_logger),
@@ -61,9 +87,20 @@ class TestParsivel(unittest.TestCase):
         mock_logger.info.assert_called_once_with(msg="Starting parsivel start sequence commands")
         mock_serial_connection.reset_input_buffer.assert_called_once()
 
-        parsivel_obj.write.assert_has_calls(expected_calls)
+        parsivel_obj.write.assert_has_calls(expected_calls_write)
+
+        expected_calls_sleep = [
+            call(1),
+            call(2),
+            call(10)
+        ]
+
+        mock_sleep.assert_has_calls(expected_calls_sleep)
 
     def test_write_success(self):
+        """
+        Good weather test for the write function
+        """
         mock_serial_connection = Mock()
         parsivel_obj = Parsivel()
         parsivel_obj.serial_connection = mock_serial_connection
@@ -76,6 +113,9 @@ class TestParsivel(unittest.TestCase):
         mock_logger.error.assert_not_called()
 
     def test_write_fail(self):
+        """
+        Bad weather test for the write function
+        """
         parsivel_obj = Parsivel()
         mock_logger = Mock()
 
@@ -85,6 +125,9 @@ class TestParsivel(unittest.TestCase):
         mock_logger.error.assert_called_once_with(msg="serial_connection not initialized")
 
     def test_read_success(self):
+        """
+        Good weather test for the read function
+        """
         parsivel_obj = Parsivel()
         mock_serial_connection = Mock()
         mock_serial_connection.readlines.return_value = "test"
@@ -97,6 +140,9 @@ class TestParsivel(unittest.TestCase):
         mock_logger.error.assert_not_called()
 
     def test_read_fail(self):
+        """
+        Bad weather test for the read function
+        """
         parsivel_obj = Parsivel()
         mock_logger = Mock()
 
@@ -106,6 +152,9 @@ class TestParsivel(unittest.TestCase):
         mock_logger.error.assert_called_once_with(msg="serial_connection not initialized")
 
     def test_get_type(self):
+        """
+        Test for the get_type function
+        """
         parsivel_obj = Parsivel()
 
         assert parsivel_obj.get_type() == "parsivel"
