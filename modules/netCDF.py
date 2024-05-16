79b553999609633b102d20bf764bd50788865c50
Jesse Vleeschdraager <J.C.Vleeschdraager@student.tudelft.nl>
2024-05-16 14:17:05 +0200

    Replace 'include_in_nc' with full, light, never

--- modules/netCDF.py
+++ modules/netCDF.py
@@ -63,8 +63,8 @@ class NetCDF:
         # --- NetCDF variables in telegram_data ---
         for key in self.telegram_objs[0].telegram_data.keys(): # pylint: disable=too-many-nested-blocks
             if key in self.config_dict['telegram_fields'].keys() and \
-                    ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') > 0) or
-                    self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') > 1):
+                    ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') != 'never') or
+                    self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') == 'full'):
                 field_dict = self.config_dict['telegram_fields'][key]
                 standard_name = field_dict['var_attrs']['standard_name']
                 netCDF_var = netCDF_rootgrp.variables[standard_name]
@@ -163,8 +163,8 @@ class NetCDF:
 
     def __set_netcdf_variable(self, key, one_var_dict, nc_group):
         self.logger.info(msg=f"creating netCDF variable {one_var_dict['var_attrs']['standard_name']}")
-        if ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') > 0) or
-            self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') > 1):
+        if ((self.full_version is True and self.config_dict['telegram_fields'][key].get('include_in_nc') != 'never') or
+            self.full_version is False and self.config_dict['telegram_fields'][key].get('include_in_nc') == 'full'):
             if one_var_dict['dtype'] != 'S4':  # can't compress variable-length str variables
                 compression_method = 'zlib'
                 # compression_method = dict(zlib=True, shuffle=True, complevel=5)
