6b290be14566a9965849683c4766317ec9b80143
Noky Soekarman <N.P.Soekarman@student.tudelft.nl>
2024-05-16 21:59:37 +0200

    Added documentation

--- test_thies_sensor_class.py
+++ test_thies_sensor_class.py
@@ -1,13 +1,19 @@
-import time
+"""
+Imports
+"""
 from unittest import mock
-
-from modules.now_time import NowTime
-from modules.sensors import Thies, SensorType
+from modules.sensors import Thies, SensorType # pylint: disable=import-error
 
 
 @mock.patch('modules.sensors.serial.Serial')
 @mock.patch('modules.sensors.sys.exit')
 def test_init_serial_connection_exception(mock_sys_exit, mock_serial):
+    """
+    Test if the init_serial_connection function raises
+    an exception when the serial connection fails
+    :param mock_sys_exit: the mocked exit function
+    :param mock_serial: the mocked Serial object
+    """
     sensor = Thies()
     logger = mock.MagicMock()
 
@@ -20,6 +26,10 @@ def test_init_serial_connection_exception(mock_sys_exit, mock_serial):
 
 @mock.patch('modules.sensors.serial.Serial')
 def test_init_serial_connection(mock_serial):
+    """
+    Test if the init_serial_connection function initializes the serial connection correctly
+    :param mock_serial: the mocked Serial object
+    """
     thies = Thies()
     logger = mock.MagicMock()
     thies.init_serial_connection(port='/dev/ttyACM0', baud=9600, logger=logger)
@@ -29,14 +39,20 @@ def test_init_serial_connection(mock_serial):
 @mock.patch('modules.sensors.NowTime')
 @mock.patch('modules.sensors.sleep', return_value=None)
 @mock.patch('modules.sensors.serial.Serial')
-def test_sensor_start_sequence(mock_serial, mock_sleep, mock_now_time):
+def test_sensor_start_sequence(mock_serial, mock_sleep, mock_now_time): # pylint: disable=unused-argument
+    """
+    Test if the sensor_start_sequence function initializes the thies sensor correctly
+    it also tests if all the commands are sent in the correct order
+    :param mock_serial: the mocked Serial object
+    :param mock_sleep: temporary argument for now
+    :param mock_now_time: the mocked NowTime object
+    """
 
     thies = Thies()
     thies.thies_id = '06'
     thies.serial_connection = mock_serial
 
     logger = mock.MagicMock()
-    mock_sleep.return_value = None
 
     now_time_instance = mock.MagicMock()
     now_time_instance.time_list = ['10', '20', '30']
@@ -61,6 +77,9 @@ def test_sensor_start_sequence(mock_serial, mock_sleep, mock_now_time):
 
 
 def test_write_fail():
+    """
+    Test if the logger writes an error when there is an exception in the write function
+    """
     thies = Thies()
     logger = mock.MagicMock()
     thies.write("test", logger)
@@ -68,6 +87,10 @@ def test_write_fail():
 
 
 def test_write_successful():
+    """
+    Test if the write function writes the message to the serial connection
+    and nothing gets written to the logger
+    """
     thies = Thies()
     logger = mock.MagicMock()
     thies.serial_connection = mock.MagicMock()
@@ -77,6 +100,9 @@ def test_write_successful():
 
 
 def test_read_fail():
+    """
+    Test if the read function writes an error to the logger when there is an exception
+    """
     thies = Thies()
     logger = mock.MagicMock()
     return_value = thies.read(logger)
@@ -85,6 +111,9 @@ def test_read_fail():
 
 
 def test_read_successful():
+    """
+    Test if the read function reads the data from the serial connection
+    """
     thies = Thies()
     logger = mock.MagicMock()
     mock_serial = mock.MagicMock()
@@ -97,16 +126,25 @@ def test_read_successful():
 
 
 def test_get_type():
+    """
+    Test if the get_type function returns the correct sensor type
+    """
     thies = Thies()
     assert thies.get_type() == SensorType.THIES
 
 
-def test_get_serial_connection():
+def test_get_serial_connection_is_none():
+    """
+    Test if the get_serial_connection function returns None when the serial connection is not initialized
+    """
     thies = Thies()
     assert thies.get_serial_connection() is None
 
 
-def test_get_serial_connection2():
+def test_get_serial_connection():
+    """
+    Test if the get_serial_connection function returns the serial connection
+    """
     thies = Thies()
     mock_serial = mock.MagicMock()
     thies.serial_connection = mock_serial
