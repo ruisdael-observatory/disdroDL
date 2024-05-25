22fa1c29b7f144e94f727c584fce8ec5574bac9d
Noky Soekarman <N.P.Soekarman@student.tudelft.nl>
2024-05-25 19:12:33 +0200

    Created reset_thies script

---
+++ reset_thies.py
@@ -0,0 +1,26 @@
+"""
+TBC
+"""
+from pathlib import Path
+from argparse import ArgumentParser
+from pydantic.v1.utils import deep_update
+from modules.util_functions import create_logger, yaml2dict  # pylint: disable=import-error
+from modules.sensors import Thies
+
+parser = ArgumentParser(description="Ruisdael: OTT Disdrometer reset. Run: python reset_parsivel.py -c config_*.yml")
+parser.add_argument('-c', '--config', required=True, help='Observation site config file. ie. -c config_008_GV.yml')
+args = parser.parse_args()
+
+wd = Path(__file__).parent
+config_dict = yaml2dict(path=wd / 'configs_netcdf' / 'config_general_parsivel.yml')
+config_dict_site = yaml2dict(path=wd / args.config)  # TODO: come from cli # pylint: disable=fixme
+config_dict = deep_update(config_dict, config_dict_site)
+
+logger = create_logger(log_dir=Path(config_dict['log_dir']),
+                       script_name=config_dict['script_name'],
+                       parsivel_name=config_dict['global_attrs']['sensor_name'])
+
+thies = Thies()
+thies.init_serial_connection(port=config_dict['port'], baud=config_dict['baud'], logger=logger)
+thies.reset_sensor(logger=logger, factory_reset=False)
+thies.close_serial_connection()
