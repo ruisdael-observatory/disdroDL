bdab4f9eb1a7b5b25c8f4532348a67fe98ddf27b
Noky Soekarman <N.P.Soekarman@student.tudelft.nl>
2024-06-07 16:08:27 +0200

    Renamed test file and removed other one

--- tests/test_reset_sensor.py
+++ tests/test_reset_sensor.py
@@ -43,7 +43,7 @@ class TestResetParsivel(unittest.TestCase):
     @patch('reset_sensor.create_logger')
     @patch('reset_sensor.yaml2dict')
     @patch('reset_sensor.Parsivel')
-    def test_main(self, mock_parsivel, mock_yaml2dict, mock_create_logger):
+    def test_main_parsivel(self, mock_parsivel, mock_yaml2dict, mock_create_logger):
         """
         Tests for the main function
         :param mock_parsivel: Mock object for the Parsivel call
@@ -83,3 +83,47 @@ class TestResetParsivel(unittest.TestCase):
         (mock_parsivel_obj.init_serial_connection.
          assert_called_once_with(port=test_conf_dict['port'], baud=test_conf_dict['baud'], logger=mock_logger))
         mock_parsivel_obj.reset_sensor.assert_called_once()
+
+    @patch('reset_sensor.create_logger')
+    @patch('reset_sensor.yaml2dict')
+    @patch('reset_sensor.Thies')
+    def test_main_thies(self, mock_thies, mock_yaml2dict, mock_create_logger):
+        """
+        Tests for the main function
+        :param mock_thies: Mock object for the Thies call
+        :param mock_yaml2dict: Mock object for the yaml2dict call
+        :param mock_create_logger: Mock object for the create_logger call
+        """
+        test_conf_dict = {
+            'log_dir': 'value1',
+            'script_name': 'value2',
+            'global_attrs': {
+                'sensor_name': 'thies',
+                'sensor_type': 'Thies Clima'
+            },
+            'port': 'value4',
+            'baud': 'value5'
+        }
+
+        mock_yaml2dict.return_value = test_conf_dict
+        mock_logger = Mock()
+        mock_create_logger.return_value = mock_logger
+
+        mock_thies_obj = Mock()
+        mock_thies.return_value = mock_thies_obj
+
+        reset_sensor.main('config_general_thies.yml')
+
+        expected_calls_yaml2dict = [
+            call(path=wd / 'configs_netcdf' / 'config_general_thies.yml')
+        ]
+
+        mock_yaml2dict.assert_has_calls(expected_calls_yaml2dict)
+        mock_thies.assert_called_once()
+        mock_create_logger.assert_called_with(log_dir=Path('value1'),
+                                              script_name='value2',
+                                              sensor_name='thies')
+
+        (mock_thies_obj.init_serial_connection.
+         assert_called_once_with(port=test_conf_dict['port'], baud=test_conf_dict['baud'], logger=mock_logger))
+        mock_thies_obj.reset_sensor.assert_called_once_with(logger=mock_logger, factory_reset=False)
