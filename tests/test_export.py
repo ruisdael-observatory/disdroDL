b80a987883e0800d5ae78156abbdb1a5c2faa244
Andre Castro <a.i.castro@tudelft.nl>
2024-09-02 15:57:50 +0200

    test_NetCDF_export_w_gaps_thies test added

--- tests/test_export.py
+++ tests/test_export.py
@@ -351,13 +351,14 @@ class EmptyExportTests(unittest.TestCase):
 
 
 @pytest.mark.usefixtures("db_insert_24h_w_empty_telegram_parsivel")
+@pytest.mark.usefixtures("db_insert_24h_w_empty_telegram_thies")
 class MissingDataExportTests(unittest.TestCase):
     """
     Class tests  export_disdrodlDB2NC.py handling of DB with empty telegrams
 
     Functions:
-    - test_NetCDF_export_w_gaps_parsivel: Verifies that running the export script for a date
-        without database entries resuls in a SystemExit.
+    - test_NetCDF_export_w_gaps_parsivel: checks that db rows w/ empty telegrams are ignored in export_disdrodlDB2NC.py
+    - test_NetCDF_export_w_gaps_thies: checks that db rows w/ empty telegrams are ignored in export_disdrodlDB2NC.py
     """
     @patch('export_disdrodlDB2NC.create_dir')
     @patch('export_disdrodlDB2NC.connect_db')
@@ -394,5 +395,36 @@ class MissingDataExportTests(unittest.TestCase):
             os.remove(output_file_path)
         os.remove(test_db_path)
 
-# TODO: same with thies
+    @patch('export_disdrodlDB2NC.create_dir')
+    @patch('export_disdrodlDB2NC.connect_db')
+    @patch('export_disdrodlDB2NC.NetCDF')
+    def test_NetCDF_export_w_gaps_thies(self, mock_NetCDF, mock_connect_db, mock_create_dir):
+        output_file_path = output_file_dir / '20240101_Test_Suite-TEST_THIES000.nc'  # see configs_netcdf/config_THIES_000_TEST.yml
+        if os.path.exists(output_file_path):
+            os.remove(output_file_path)
 
+        test_db_path_str = 'sample_data/test_thies.db'
+        test_db_path = Path(test_db_path_str)
+        data_points_24h = 1440  # (60min * 24h)
+        mock_args = Mock()
+        mock_args.config = 'configs_netcdf/config_THIES_000_TEST.yml'  # db_path sample_data/test_thies.db
+        mock_args.date = '2024-01-01'
+        mock_args.version = 'full'
+        mock_connect_db.return_value = connect_db(dbpath=str(test_db_path))
+        mock_create_dir.return_value = create_dir(path=output_file_dir)
+        mock_NetCDF.side_effect = side_effect
+        export_disdrodlDB2NC.main(mock_args)
+
+        # test netcdf:  only half of data points should have be included
+        assert output_file_path.exists()
+        rootgrp = Dataset(output_file_path, 'r', format="NETCDF4")
+        netCDF_var_time = rootgrp.variables['time']
+        netCDF_var_time_data = netCDF_var_time[:].data
+        assert len(netCDF_var_time_data) == data_points_24h / 2
+        netCDF_var_tsensor = rootgrp.variables['interior_temperature']
+        netCDF_var_tsensor_data = netCDF_var_tsensor[:].data
+        assert len(netCDF_var_tsensor_data) == data_points_24h / 2
+        assert netCDF_var_tsensor_data[0] == 23 and netCDF_var_tsensor_data[1] == 23
+        if os.path.exists(output_file_path):
+            os.remove(output_file_path)
+        os.remove(test_db_path)
